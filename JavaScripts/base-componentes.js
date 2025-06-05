693244169c0843455f9fc988d0ccb8ecf47aa686
ThalissonM <manoelthalisson28@gmail.com>
2025-06-04 21:39:06 -0300

    Header responsivo
    Colocado o menu responsivo para ficar em hambúrguer quando estiver em telas abaixo de 1024px

--- JavaScripts/base-componentes.js
+++ JavaScripts/base-componentes.js
@@ -1,11 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Carrega o header
     fetch("../Components/header.html")
         .then(res => res.text())
-        .then(data => document.getElementById("header").innerHTML = data);
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
 
+            // Só agora o botão existe! Então adicionamos o evento aqui:
+            const menu = document.getElementById("menu");
+            const nav = document.querySelector("nav");
+
+            if (menu && nav) {
+                menu.addEventListener("click", () => {
+                    nav.classList.toggle("active");
+                    menu.classList.toggle("active");
+                });
+            }
+        });
+
+    // Carrega o footer
     fetch("../Components/footer.html")
         .then(res => res.text())
         .then(data => document.getElementById("footer").innerHTML = data);
 
+    // Define o favicon
     document.getElementById("favicon").href = "../Assets/Logo (1).svg";
 });
